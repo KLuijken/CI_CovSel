5fa9dd4326e7756b203b541ec51a4a77f67e68f7
KLuijken <46456987+KLuijken@users.noreply.github.com>
2020-01-30 11:42:59 +0100

    Correct sampling strategy

--- createdseeds.R
+++ createdseeds.R
@@ -7,7 +7,7 @@
 
 # S1, seeds
 set.seed(1)
-seedsS1 <- matrix(sample(1:10000000, size=nrow(S1)*nsim), nrow=nrow(S1))
+seedsS1 <- matrix(sample(1:10000000, size=nrow(S1)*nsim, replace = F), nrow=nrow(S1))
 saveseedsS1 <- lapply(1:nrow(seedsS1), FUN= function(x) saveRDS(seedsS1[x,],file = paste0(filepath,"Siminput/S1/seedsS1_",x,".rds")))
 set.seed(11)
 bsseedsS1 <- array(replicate(nrow(S1)*nsim,sample(1:1000000000, size = B, replace=F)),dim=c(nrow=B,ncol=nsim,height=nrow(S1)))
@@ -16,7 +16,7 @@ savebsseedsS1 <- lapply(1:(dim(bsseedsS1)[3]), FUN= function(x) saveRDS(bsseedsS
 
 # S2, seeds
 set.seed(2)
-seedsS2 <- matrix(sample(1:10000000, size=nrow(S2)*nsim), nrow=nrow(S2))
+seedsS2 <- matrix(sample(1:10000000, size=nrow(S2)*nsim, replace = F), nrow=nrow(S2))
 saveseedsS2 <- lapply(1:nrow(seedsS2), FUN= function(x) saveRDS(seedsS2[x,],file = paste0(filepath,"Siminput/S2/seedsS2_",x,".rds")))
 set.seed(22)
 bsseedsS2 <- array(replicate(nrow(S2)*nsim,sample(1:1000000000, size = B, replace=F)),dim=c(nrow=B,ncol=nsim,height=nrow(S2)))
@@ -25,7 +25,7 @@ savebsseedsS2 <- lapply(1:(dim(bsseedsS2)[3]), FUN= function(x) saveRDS(bsseedsS
 
 # S3, seeds
 set.seed(3)
-seedsS3 <- matrix(sample(1:10000000, size=nrow(S3)*nsim), nrow=nrow(S3))
+seedsS3 <- matrix(sample(1:10000000, size=nrow(S3)*nsim, replace = F), nrow=nrow(S3))
 saveseedsS3 <- lapply(1:nrow(seedsS3), FUN= function(x) saveRDS(seedsS3[x,],file = paste0(filepath,"Siminput/S3/seedsS3_",x,".rds")))
 set.seed(33)
 bsseedsS3 <- array(replicate(nrow(S3)*nsim,sample(1:1000000000, size = B, replace=F)),dim=c(nrow=B,ncol=nsim,height=nrow(S3)))
