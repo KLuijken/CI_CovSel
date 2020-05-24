368437f607441501982aa010a8954223b9d8d381
Anna Lohmann <anna@lohmann-web.net>
2020-05-24 11:02:22 +0200

    Start else on last line of if. Add {} to if.

--- rcode/analyses/helpers/be_firth.R
+++ rcode/analyses/helpers/be_firth.R
@@ -20,20 +20,20 @@ backwardf <- function (object, scope, steps = 1000, slstay, trace = TRUE,
   }
   if (missing(scope)) 
     scope <- attr(terms(working), "term.labels")
-  while (istep < steps & working$df >= 2) {
+  while (istep < steps & working$df >= 2){
     istep <- istep + 1
     mat <- drop1(working)
     inscope <- match(scope, rownames(mat))  
     inscope <- inscope[!is.na(inscope)]
-    if (all(mat[inscope, 3] < slstay))      ### Bug-fix from original backward() function in logistf package
+    if (all(mat[inscope, 3] < slstay)){      ### Bug-fix from original backward() function in logistf package
                                             ### added inscope to subset the p-values
-      break
+      break}
     removal <- rownames(mat)[mat[, 3] == max(mat[inscope, 
                                                  3])]
     newform = as.formula(paste("~.-", removal))
-    if (working$df == 2 | working$df == mat[mat[, 3] == max(mat[, 3]), 2]) 
-      working <- update(working, formula = newform, pl = FALSE)
-    else working <- update(working, formula = newform)
+    if (working$df == 2 | working$df == mat[mat[, 3] == max(mat[, 3]), 2]){
+      working <- update(working, formula = newform, pl = FALSE)}else{ 
+        working <- update(working, formula = newform)}
     if (trace) {
       cat("drop1:\n")       
       print(mat)           
