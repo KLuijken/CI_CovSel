1059a965b86dd78ead7189811829b00bc31d432f
KLuijken <k.luijken@lumc.nl>
2020-06-03 21:37:15 +0200

    First attempt to obtain true MRR using integration

--- rcode/analyses/helpers/MRR.R
+++ rcode/analyses/helpers/MRR.R
@@ -2,7 +2,10 @@
 # Causal inference covariate selection
 # K Luijken
 #
-# Helper function to estimate Marginal Risk Ratio
+# Helper function to obtain Marginal Risk Ratio
+#------------------------------------------------------------------------------#
+
+# Estimate Marginal Risk Ratio from logistic regression models ----
 #------------------------------------------------------------------------------#
 
 estimate_marginals <- function(data, int, modelcoefs){
@@ -22,4 +25,35 @@ estimate_marginals <- function(data, int, modelcoefs){
     MRR <- MOR <- NA)
     
   return(list(MRR = MRR, MOR = MOR))
+}
+
+# Obtain true Marginal Risk Ratio by numerical integration ----
+#------------------------------------------------------------------------------#
+
+integrate_MRR <- function(nL, bYA, bL_const, bAL1, bAL2, bAL3, bYL1, bYL2, bYL3, Yint, rhoL){
+  var.L1.star <- (nL/2) + (nL/2)*((nL/2)-1)*rhoL
+  var.L2.star <- (nL/6) + (nL/6)*((nL/6)-1)*rhoL
+  var.L3.star <- (nL/6) + (nL/6)*((nL/6)-1)*rhoL
+  var.L4.star <- (nL/6) + (nL/6)*((nL/6)-1)*rhoL
+  cov.L       <- (nL/2)*(nL/6)*(nL/6)*(nL/6)*rhoL
+  sigma       <- matrix(cov.L,ncol=4,nrow=4)
+  diag(sigma) <- c(var.L1.star,var.L2.star,var.L3.star,var.L3.star)
+  dx          <- 0.1
+  values      <- seq(-4,4,by=dx)
+  x           <- expand.grid(values,values,values,values)
+  dL          <- mvrnorm(nrow(x), mu=rep(0, times = 4), Sigma = sigma)
+  
+  pY0     		<- plogis((Yint + bL_const*x[,1] + 
+                         bYL1*x[,2] + bYL2*x[,3] + 
+                         bYL3*x[,4]))*(dL*dx) 
+  pY1     		<- plogis((Yint + bL_const*x[,1] + 
+                         bYL1*x[,2] + bYL2*x[,3] + 
+                         bYL3*x[,4] + 
+                         bYA))*(dL*dx) 
+  
+  MRR <- mean(pY1)/mean(pY0)
+  MOR <- (mean(pY1) * (1- mean(pY0)))/((1-mean(pY1)) * mean(pY0))
+  
+  
+  return(list(MRR=MRR, MOR=MOR))
 }
