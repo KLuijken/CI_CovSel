55f173e1530427462fd5ceebe4f0283cca9d0453
KLuijken <k.luijken@lumc.nl>
2020-06-16 16:23:00 +0200

    Improve unadjusted analysis

--- rcode/analyses/helpers/unadjusted.R
+++ rcode/analyses/helpers/unadjusted.R
@@ -2,15 +2,15 @@
 # Causal inference covariate selection
 # K Luijken
 #
-# Helper function perform unadjusted analysis [note: not added yet]
+# Helper function perform unadjusted analysis 
 #------------------------------------------------------------------------------#
 
 analyse_unadjusted <- function(data){
   pY1 <- mean(data$Y[data$A==1])
   pY0 <- mean(data$Y[data$A==0])
   
-  MRR <- mean(pY1)/mean(pY0)
-  MOR <- (mean(pY1) * (1-mean(pY0))) / (mean(pY0) * (1-mean(pY1)))
+  MRR <- pY1/pY0
+  MOR <- (pY1 * (1-pY0)) / (pY0 * (1-pY1))
 
   return(list(MRR=MRR,
               MOR=MOR))
