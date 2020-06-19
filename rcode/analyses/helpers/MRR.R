556e295a773ef6820bbac73b210a7df6d6ed6d38
KLuijken <k.luijken@lumc.nl>
2020-06-19 09:47:44 +0200

    Remove redundant arguments numerical integration

--- rcode/analyses/helpers/MRR.R
+++ rcode/analyses/helpers/MRR.R
@@ -26,7 +26,7 @@ estimate_marginals <- function(data, int, modelcoefs){
 # Obtain true Marginal Ratios by numerical integration ----
 #------------------------------------------------------------------------------#
 
-integrate_marginals <- function(nL, bYA, bL_const, bAL1, bAL2, bAL3, bYL1, bYL2, bYL3, Yint, rhoL){
+integrate_marginals <- function(nL, bYA, bL_const, bYL1, bYL2, bYL3, Yint, rhoL){
   var.L1.star <- (nL/2) + (nL/2)*((nL/2)-1)*rhoL
   var.L2.star <- (nL/6) + (nL/6)*((nL/6)-1)*rhoL
   var.L3.star <- (nL/6) + (nL/6)*((nL/6)-1)*rhoL
