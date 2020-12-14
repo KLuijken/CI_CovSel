1f5cde5f3c5f0c1fcda440f126e6963efe84e96d
KLuijken <k.luijken@lumc.nl>
2020-12-14 10:19:19 +0100

    Update summarize_simulation.R

--- rcode/sumsim/summarize_simulation.R
+++ rcode/sumsim/summarize_simulation.R
@@ -9,52 +9,79 @@
 
 # Libraries  ----
 #------------------------------------------------------------------------------#
-source("./rcode/analyses/helpers/select_scenarios.R")
 
 # Helpers  ----
 #------------------------------------------------------------------------------#
+# In the helper functions, we select na.rm = TRUE to deal with the NAs that are 
+# introduced because in 10 datasets the effect could not be estimated using 
+# Maximum Likelihood.
+
+Value <- function(results, 
+                  estimator, # string
+                  transformation){
+  bias       <- c(by(results, results[['model']],
+                     function(x) mean(transformation(unlist(x[[estimator]])), na.rm = TRUE)))
+  
+  return(bias)
+}
+
 Bias <- function(results, 
                  estimator, # string
-                 true_value){
+                 true_value,
+                 transformation){
   bias       <- c(by(results, results[['model']],
-             function(x) mean(log(unlist(x[[estimator]])), na.rm = TRUE)-log(true_value)))
+             function(x) mean(transformation(unlist(x[[estimator]])), na.rm = TRUE)-transformation(true_value)))
   
   return(bias)
 }
 
 EmpSE <- function(results,
-                  estimator){
-  count <- sum(!is.na(results[[estimator]]))
+                  estimator,
+                  transformation,
+                  rep){
   empSE <- c(by(results,results[['model']], function(x) 
-    sqrt(sum((log(unlist(x[[estimator]])) - mean(log(unlist(x[[estimator]])), na.rm = TRUE))^2)/(count-1))))
+    sqrt(sum((transformation(unlist(x[[estimator]])) - mean(transformation(unlist(x[[estimator]])), na.rm = TRUE))^2, na.rm = TRUE)/(rep-1))))
   
   return(empSE)
 }
 
 EmpVar <- function(results,
-                   estimator){
-  count <- sum(!is.na(results[[estimator]]))
+                   estimator,
+                   transformation,
+                   rep){
   empvar <- c(by(results,results[['model']], function(x) 
-    sum((log(unlist(x[[estimator]])) - mean(log(unlist(x[[estimator]])), na.rm = TRUE))^2)/(count-1)))
+    sum((transformation(unlist(x[[estimator]])) - mean(transformation(unlist(x[[estimator]])), na.rm = TRUE))^2, na.rm = TRUE)/(rep-1)))
   
   return(empvar)
 }
 
 MSE <- function(results, 
                 estimator,
-                true_value){
+                true_value,
+                transformation){
   mse        <- c(by(results, results[['model']],
-              function(x) mean((log(unlist(x[[estimator]]))-log(true_value))^2, na.rm = TRUE)))
+              function(x) mean((transformation(unlist(x[[estimator]]))-transformation(true_value))^2, na.rm = TRUE)))
   
   return(mse)
 }
 
-na_count <- function(results, 
+count_missings <- function(results, 
                      estimator){
-  na_count <- c(by(results,results[['model']],
+  count <- c(by(results,results[['model']],
                    function(x) sum(is.na(x[[estimator]]))))
+  return(count)
 }
 
+count_warnings <- function(results){
+  count <- c(by(results,results[['model']],
+                function(x) sum(!is.na(x[['mod_warning']]))))
+  return(count)
+}
+
+show_warnings <- function(results){
+  paste(by(results,results[['model']],
+                function(x) unique(x[['mod_warning']])))
+}
 
 
 # Summarize simulations  ----
@@ -62,30 +89,47 @@ na_count <- function(results,
 
 # Function to summarize a single scenario. 
 sum_one_scenario <- function(datagen_scenario, method, pcutoff,
-                estimator){
+                estimator, rep){
   scen_num   <- datagen_scenario[['scen_num']]
   
   # load results
   results    <- readRDS(paste0("./data/raw/",method,"_",pcutoff,"/S",scen_num,".rds"))
   
-  # store true value
-  ifelse(estimator == "COR",{
+  # store true value and corresponding transformation
+  ifelse(estimator == "A",{
     true_value <- datagen_scenario[['bYA']]
+    trans      <- identity
   },{
     truth      <- readRDS(paste0("./data/raw/truth/S",scen_num,".rds"))
     true_value <- truth[[estimator]]
+    trans      <- log
   })
   
-  bias <- Bias(results = results, estimator = estimator, true_value = true_value)
-  empSE <- EmpSE(results = results, estimator = estimator)
-  empVar <- EmpVar(results = results, estimator = estimator)
-  mse <- MSE(results = results, estimator = estimator, true_value = true_value)
-  na_count <- na_count(results = results, estimator = estimator)
+  value <- Value(results = results, estimator = estimator, transformation = trans)
+  bias <- Bias(results = results, estimator = estimator, true_value = true_value, transformation = trans)
+  empSE <- EmpSE(results = results, estimator = estimator, rep = rep, transformation = trans)
+  empVar <- EmpVar(results = results, estimator = estimator, rep = rep, transformation = trans)
+  mse <- MSE(results = results, estimator = estimator, true_value = true_value, transformation = trans)
+  na_count <- count_missings(results = results, estimator = estimator)
+  war_count <- count_warnings(results = results)
+  war_message <- show_warnings(results = results)
 
   out <- data.table(scen_num, names(bias),
-                    paste0(method,"_",pcutoff), cbind(bias, empSE, empVar, mse, na_count))
+                    paste0(method,"_",pcutoff),
+                    value,
+                    bias,
+                    empSE,
+                    empVar,
+                    mse,
+                    na_count,
+                    war_count,
+                    war_message)
+  
   colnames(out) <- c("scen_num", "model", "method",
-                     paste0(c("bias_","empSE_","empvar_","MSE_"),estimator),"removed_replications")
+                     paste0(c("value_","bias_","empSE_","empvar_","MSE_"),estimator),
+                     "removed_replications",
+                     "warnings_count",
+                     "warnings_message")
 
   return(out)
 }
@@ -96,15 +140,17 @@ sum_one_scenario <- function(datagen_scenario, method, pcutoff,
 sum_multiple_scenarios <- function(method,
                                    pcutoff,
                                    use_datagen_scenarios,
-                                   estimator){
+                                   estimator, 
+                                   rep){
   output <- do.call(rbind, apply(use_datagen_scenarios,
                                  MARGIN = 1,
                                  sum_one_scenario,
                                   method = method,
                                   pcutoff = pcutoff,
-                                  estimator = estimator))
+                                  estimator = estimator,
+                                  rep = rep))
   
   dir.create(file.path(".","data","summarised"), recursive = TRUE)
-  saveRDS(output,file = paste0("./data/summarised/",method,"_",pcutoff,".rds"))
+  saveRDS(output,file = paste0("./data/summarised/",method,"_",pcutoff,"_",estimator,".rds"))
 }
 
