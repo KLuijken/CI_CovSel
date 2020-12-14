6b6f13b3d5048459d252b918430953d77e042b2b
KLuijken <k.luijken@lumc.nl>
2020-12-14 10:20:36 +0100

    Update diagnostics.R

--- rcode/sumsim/diagnostics.R
+++ rcode/sumsim/diagnostics.R
@@ -18,11 +18,25 @@ diagn_freqs <- function(use_datagen_scenarios){
                                   "_Yint = ",use_datagen_scenarios[['Yint']])
   
   for(i in 1:nrow(use_datagen_scenarios)){
-    results <- readRDS(paste0("./data/raw/descriptives/S",use_datagen_scenarios[i,"scen_num"],".rds"))
+    results <- readRDS(paste0("./data/raw/descriptives/S", i,".rds"))
     frequency_A[,i] <- results$freq_A
     frequency_Y[,i] <- results$freq_Y
     
   }
+  
+  ### Overall frequencies
+  hist_A <- unlist(c(frequency_A))
+  pdf("./data/diagnostics/FrequencyA_hist.pdf")
+  hist(hist_A, xlab = "frequency A")
+  dev.off()
+  
+  hist_Y_0.2 <- unlist(c(frequency_Y[which(use_datagen_scenarios$eventrate == 0.2)]))
+  hist_Y_0.03 <- unlist(c(frequency_Y[which(use_datagen_scenarios$eventrate == 0.03)]))
+  pdf("./data/diagnostics/FrequencyY_hist.pdf")
+  hist(hist_Y_0.2, xlab = "frequency Y")
+  hist(hist_Y_0.03, xlab = "frequency Y")
+  dev.off()
+  
   
   # Create output directory
   dir.create(file.path(".","data","diagnostics"), recursive = TRUE)
@@ -44,33 +58,26 @@ diagn_freqs <- function(use_datagen_scenarios){
 #------------------------------------------------------------------------------#
 
 diagn_warnings_onemethod <- function(use_datagen_scenarios, method, pcutoff){
+  write.table("warnings",
+              file = paste0("./data/diagnostics/warningsmodel_",method,".txt"), 
+              append = F,
+              row.names = F,
+              col.names = F)
+  
   for(i in 1:nrow(use_datagen_scenarios)){
     results <- readRDS(paste0("./data/raw/",method,"_",pcutoff,"/S",
                               use_datagen_scenarios[i,"scen_num"],".rds"))
     
     # Store model warnings
     if(!identical(results$mod_warning[!is.na(results$mod_warning)],
-                             character(0))){
+                             logical(0))){
       write.table(paste0(results$mod_warning[!is.na(results$mod_warning)],
                          " in Scenario ", 
                          results$scen_num[!is.na(results$mod_warning)],
                          " Seed ", 
                          results$seed[!is.na(results$mod_warning)]),
-                  file = "./data/diagnostics/warningsmodel.txt", 
+                  file = paste0("./data/diagnostics/warningsmodel_",method,".txt"), 
                   append = T,
-                  row.names = F,
-                  col.names = F)} 
-    
-    # Store intercept model warnings
-      if(!identical(results$intmod_warning[!is.na(results$intmod_warning)],
-                             character(0))){
-      write.table(paste0(results$intmod_warning[!is.na(results$intmod_warning)],
-                         " in Scenario ", 
-                         results$scen_num[!is.na(results$intmod_warning)],
-                         " Seed ", 
-                         results$seed[!is.na(results$intmod_warning)]),
-                  file = "./data/diagnostics/warningsintmodel.txt", 
-                  append = T, 
                   row.names = F,
                   col.names = F)} 
     
